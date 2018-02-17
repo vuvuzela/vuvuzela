0962ff464ddfddfa6542a7582d9d1680dcf122fe
David Lazar <lazard@csail.mit.edu>
2018-02-17 17:29:20 -0500

    cmd/vuvuzela-client: split code into multiple files

---
+++ cmd/vuvuzela-client/connect.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"vuvuzela.io/alpenhorn/pkg"
+)
+
+func (gc *GuiClient) Connect() {
+	if gc.CheckPKGStatus() {
+		gc.EnsureConnected()
+	}
+}
+
+func (gc *GuiClient) EnsureConnected() {
+	gc.connectOnce.Do(func() {
+		gc.lastSeenConvoReady = make(chan struct{})
+
+		go gc.connectLoop("AddFriend", gc.alpenhornClient.ConnectAddFriend)
+		go gc.connectLoop("Convo", gc.convoClient.ConnectConvo)
+		go func() {
+			// The dialing protocol needs a rough estimate of the current
+			// convo round number, so wait until we hear back from the
+			// convo protocol before starting dialing.
+			<-gc.lastSeenConvoReady
+			gc.connectLoop("Dialing", gc.alpenhornClient.ConnectDialing)
+		}()
+	})
+}
+
+const connectRetry = 10 * time.Second
+
+func (gc *GuiClient) connectLoop(service string, connectFunc func() (chan error, error)) {
+	var prevErr error
+	for {
+		disconnect, err := connectFunc()
+		if err != nil {
+			if prevErr == nil || (err.Error() != prevErr.Error()) {
+				// Don't repeat the same error message over and over again.
+				gc.WarnfSync("Error connecting to %s service: %s (retrying every %s)\n", service, err, connectRetry)
+				prevErr = err
+			}
+			time.Sleep(connectRetry)
+			continue
+		}
+		gc.WarnfSync("Connected to %s service!\n", service)
+		prevErr = nil
+		err = <-disconnect
+		gc.WarnfSync("Disconnected from %s service: %s (reconnecting in %s)\n", service, err, connectRetry)
+		time.Sleep(connectRetry)
+	}
+}
+
+func (gc *GuiClient) CheckPKGStatus() bool {
+	pkgStats := gc.alpenhornClient.PKGStatus()
+	var numOK, numUnregistered int
+	for _, st := range pkgStats {
+		if st.Error == nil {
+			numOK++
+			continue
+		}
+		switch err := st.Error.(type) {
+		case pkg.Error:
+			if err.Code == pkg.ErrNotRegistered {
+				numUnregistered++
+				continue
+			}
+		}
+	}
+
+	if numUnregistered == len(pkgStats) {
+		gc.WarnfSync("Username %q not registered. Visit https://vuvuzela.io to get started.\n", gc.alpenhornClient.Username)
+		return false
+	} else if numOK != len(pkgStats) {
+		buf := new(bytes.Buffer)
+		for _, st := range pkgStats {
+			fmt.Fprintf(buf, " ·  PKG %s: %s\n", st.Server.Address, statusString(st.Error))
+		}
+		gc.WarnfSync("Connection error: inconsistent PKG status for %q:\n%s", gc.alpenhornClient.Username, buf.String())
+		gc.WarnfSync("Type /connect after resolving the issue to try again.\n")
+		return false
+	}
+
+	return true
+}
+
+func (gc *GuiClient) RegisterAll(token string) {
+	pkgStats := gc.alpenhornClient.PKGStatus()
+	buf := new(bytes.Buffer)
+	numOK := 0
+	for _, st := range pkgStats {
+		fmt.Fprintf(buf, " ·  ")
+		if st.Error == nil {
+			fmt.Fprintf(buf, "PKG %s: OK (already registered)\n", st.Server.Address)
+			numOK++
+			continue
+		}
+		pkgErr, ok := st.Error.(pkg.Error)
+		if !ok {
+			fmt.Fprintf(buf, "PKG %s: %s\n", st.Server.Address, st.Error)
+			continue
+		}
+		if pkgErr.Code != pkg.ErrNotRegistered {
+			fmt.Fprintf(buf, "PKG %s: %s\n", st.Server.Address, pkgErr)
+			continue
+		}
+		err := gc.alpenhornClient.Register(st.Server, token)
+		if err != nil {
+			fmt.Fprintf(buf, "PKG %s: failed to register: %s\n", st.Server.Address, err)
+			continue
+		}
+		fmt.Fprintf(buf, "PKG %s: OK (registered)\n", st.Server.Address)
+		numOK++
+	}
+	gc.WarnfSync("Registration status for %q:\n%s", gc.alpenhornClient.Username, buf.String())
+	if numOK == len(pkgStats) {
+		// Don't require the user to type /connect after successful registration.
+		gc.EnsureConnected()
+	}
+}
+
+func statusString(err error) string {
+	if err == nil {
+		return "OK"
+	}
+	switch err := err.(type) {
+	case pkg.Error:
+		switch err.Code {
+		case pkg.ErrNotRegistered:
+			return "Username not registered"
+		case pkg.ErrInvalidSignature:
+			return "Invalid signature (username taken)"
+		}
+	}
+	return err.Error()
+}
---
+++ cmd/vuvuzela-client/notify.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gen2brain/beeep"
+)
+
+func notify(format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	beeep.Notify("Vuvuzela", msg, "")
+}
+
+var notifyChan = make(chan string, 1)
+var notifyResetChan = make(chan struct{}, 1)
+
+func resetNotifyTimer() {
+	select {
+	case notifyResetChan <- struct{}{}:
+	default:
+	}
+}
+
+func seldomNotify(format string, args ...interface{}) {
+	select {
+	case notifyChan <- fmt.Sprintf(format, args...):
+	default:
+	}
+}
+
+func init() {
+	go seldomNotifyLoop()
+}
+
+func seldomNotifyLoop() {
+	duration := 4 * time.Minute
+	timer := time.NewTimer(duration)
+	for {
+		select {
+		case <-notifyResetChan:
+			if !timer.Stop() {
+				<-timer.C
+			}
+			timer.Reset(duration)
+		case msg := <-notifyChan:
+			select {
+			case <-timer.C:
+				beeep.Notify("Vuvuzela", msg, "")
+				timer.Reset(duration)
+			default:
+			}
+		}
+	}
+}
