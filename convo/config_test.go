c8e0d8ce006c49556523b24a1004b0b7d3e02bbe
David Lazar <lazard@csail.mit.edu>
2018-02-18 08:26:26 -0500

    convo: add missing copyright header

--- convo/config_test.go
+++ convo/config_test.go
@@ -1,3 +1,7 @@
+// Copyright 2017 David Lazar. All rights reserved.
+// Use of this source code is governed by the GNU AGPL
+// license that can be found in the LICENSE file.
+
 package convo
 
 import (
